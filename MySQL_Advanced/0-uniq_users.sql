42584f0bb059af5f356b15749c162745659f54fb
1brah1m0f <sixiibrahimov217@gmail.com>
2026-03-25 00:25:15 +0400

    create users table with id, email, and name fields

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,1 +1,5 @@
-CREATE TABLE Users;
+CREATE TABLE IF NOT EXISTS users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, --not null = tekrarlana bilmez, auto.. = artir her defe primary key = unikal olsun
+  email VARCHAR(255) NOT NULL UNIQUE, --tekrarlana bilmez
+  name VARCHAR(255)
+);
