8d1898bcf2338b7b2e8f20e2f4e260a415e3046b
1brah1m0f <sixiibrahimov217@gmail.com>
2026-03-25 00:26:35 +0400

    add comment to clarify purpose of the SQL script

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,4 @@
+-- hm what is that 
 CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, --not null = tekrarlana bilmez, auto.. = artir her defe primary key = unikal olsun
   email VARCHAR(255) NOT NULL UNIQUE, --tekrarlana bilmez
